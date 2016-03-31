129271beb8ea11b8c25970c55c6ecaf8617efb31
Julien Carsique <jcarsique@nuxeo.com>
2016-03-31 20:58:44 +0200

    NXBT-982: use project version instead of pom version
    Fix hardcoded version in src/it/test-profile/postbuild.groovy

--- src/it/test-profile/postbuild.groovy
+++ src/it/test-profile/postbuild.groovy
@@ -1,9 +1,9 @@
 def buildLog = new File(basedir, "build.log");
 assert buildLog.exists()
 activeProfiles = "Active Maven profiles:\n"+
-"it-repo (source: external)\n"+
-"profile-5 (source: org.nuxeo.build:test-profile-module:2.0.5-SNAPSHOT)\n"+
-"profile-6 (source: org.nuxeo.build:test-profile-module:2.0.5-SNAPSHOT)\n"+
-"profile-1 (source: org.nuxeo.build:test-profile-parent:2.0.5-SNAPSHOT)\n"+
-"profile-4 (source: org.nuxeo.build:test-profile-parent:2.0.5-SNAPSHOT)"
+        "it-repo (source: external)\n"+
+        "profile-5 (source: org.nuxeo.build:test-profile-module:"+projectVersion+")\n"+
+        "profile-6 (source: org.nuxeo.build:test-profile-module:"+projectVersion+")\n"+
+        "profile-1 (source: org.nuxeo.build:test-profile-parent:"+projectVersion+")\n"+
+        "profile-4 (source: org.nuxeo.build:test-profile-parent:"+projectVersion+")"
 assert buildLog.text.contains(activeProfiles)
